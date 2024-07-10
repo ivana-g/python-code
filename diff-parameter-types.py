d710bafc3aff379e408da567e3ac6f656d5e0b3d
stardust <igjorgeva@gmail.com>
2024-07-10 11:02:58 -0400

    Create diff-parameter-types.py

---
+++ diff-parameter-types.py
@@ -0,0 +1,9 @@
+# defining different parameter types 
+
+def some_func(p1, p2, *args, k, **kwargs):
+  print("positional-or-keyword:.....{}, {}".format(p1, p2))
+  print("var-positional (*args):....{}".format(args))
+  print("keyword:...................{}".format(k))
+  print("var-keyword:...............{}".format(kwargs))
+
+some_func(1, 2, 3, 4, 5, 9, k=6, key1=7, key2=8)
